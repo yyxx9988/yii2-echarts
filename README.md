908de8595a12d3381583c8e67e4301036160f2d6
zzzeep <zzzeep@qq.com>
2017-03-14 10:57:53 +0800

    fixed mutiple fileds info

--- README.md
+++ README.md
@@ -15,15 +15,25 @@ $ composer require "yyxx9988/yii2echarts"
 # Usage
 
 ```php
-use \yyxx9988\yii2echarts\ECharts;
+use yyxx9988\yii2echarts\ECharts;
 
 $echarts = new ECharts();
 
 $result = $echarts->setTableName('test_tb')
-    ->setFields(['f1', 'f2'])
-    ->setTitleText('test title')
-    ->setTitleSub('test sub title')
-    ->setSeriesName('test series name')
+    ->setFields([
+        [
+            'name' => 'f1',
+            'titleText' => 'f1 titleText',
+            'titleSub' => 'f1 titleSub',
+            'seriesName' => 'f1 seriesName',
+        ],
+        [
+            'name' => 'f2',
+            'titleText' => 'f2 titleText',
+            'titleSub' => 'f2 titleSub',
+            'seriesName' => 'f2 seriesName',
+        ],
+    ])
     ->generatePie();
 
 echo json_encode($result);
--- src/ECharts.php
+++ src/ECharts.php
@@ -4,17 +4,29 @@ namespace yyxx9988\yii2echarts;
 
 use Yii;
 use yii\db\Query;
+use yii\helpers\ArrayHelper;
 
 class ECharts
 {
+    /**
+     * @var string
+     */
     public $tableName;
+
+    /**
+     * @var array
+     */
     public $fields = [];
-    public $error;
 
-    public $titleText;
-    public $titleSub;
-    public $seriesName;
+    /**
+     * @var string
+     */
+    private $_error;
 
+    /**
+     * @param string $tableName
+     * @return $this
+     */
     public function setTableName($tableName)
     {
         $this->tableName = $tableName;
@@ -22,71 +34,63 @@ class ECharts
         return $this;
     }
 
+    /**
+     * @param array $fields
+     * @return $this
+     */
     public function setFields($fields)
     {
-        if (is_array($fields)) {
+        if (ArrayHelper::isAssociative($fields)) {
             $this->fields = $fields;
         }
 
         return $this;
     }
 
-    public function setTitleText($titleText)
-    {
-        $this->titleText = $titleText;
-
-        return $this;
-    }
-
-    public function setTitleSub($titleSub)
-    {
-        $this->titleSub = $titleSub;
-
-        return $this;
-    }
-
-    public function setSeriesName($seriesName)
-    {
-        $this->seriesName = $seriesName;
-
-        return $this;
-    }
-
+    /**
+     * @return string
+     */
     public function getError()
     {
-        return $this->error;
+        return $this->_error;
     }
 
+    /**
+     * @return array|bool
+     */
     public function generatePie()
     {
         $result = [];
 
         $Schema = Yii::$app->getDb()->getTableSchema($this->tableName);
         if (!$Schema) {
-            $this->error = "table $this->tableName does not exists";
+            $this->_error = "table $this->tableName does not exists";
             return false;
         }
+
         $columnNames = $Schema->getColumnNames();
         if (count($this->fields) > 0) {
-            $columnNames = array_intersect($columnNames, $this->fields);
+            $columnNames = array_intersect($columnNames, array_column($this->fields, 'name'));
         }
         foreach ($columnNames as $v) {
             $col = $Schema->getColumn($v);
             // verify dbType
             if (strpos($col->dbType, 'tinyint(1)') === false) {
-                $this->error = "column $v's dbType should be tinyint(1)";
+                $this->_error = "column $v's dbType should be tinyint(1)";
                 return false;
             }
             // verify comment
             if (!preg_match('#^(.*[^ ])\s+(.*)#u', trim($col->comment), $matches)) {
-                $this->error = "unable to verify comment ($col->comment)";
+                $this->_error = "unable to verify comment ($col->comment)";
                 return false;
             }
 
+            $key = array_search($v, array_column($this->fields, 'name'));
+
             $result[$v] = [
                 'title' => [
-                    'text' => $this->titleText ? $this->titleText : 'Undefined titleText',
-                    'subtext' => $this->titleSub ? $this->titleSub : '',
+                    'text' => $this->fields[$key]['titleText'] ? $this->fields[$key]['titleText'] : 'Undefined titleText',
+                    'subtext' => $this->fields[$key]['titleSub'] ? $this->fields[$key]['titleSub'] : '',
                     'x' => 'center',
                 ],
                 'tooltip' => [
@@ -99,7 +103,7 @@ class ECharts
                     'data' => [],
                 ],
                 'series' => [
-                    'name' => $this->seriesName ? $this->seriesName : 'Undefined seriesName',
+                    'name' => $this->fields[$key]['seriesName'] ? $this->fields[$key]['seriesName'] : 'Undefined seriesName',
                     'type' => 'pie',
                     'radius' => '55%',
                     'center' => ['50%', '60%'],
